738765156c25257d4d6315e9a47506445ac3aab6
U-Shamitha <shamitha19102002@gmail.com>
2024-02-10 23:49:37 +0530

    first commit

--- frontend/src/axios/axiosConfig.js
+++ frontend/src/axios/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   // baseURL: 'http://localhost:5000', // Set your base URL here
-  baseURL: 'https://onlinecourses-server.onrender.com'
+  baseURL: 'https://e-learning-server-7fc6.onrender.com'
 });
 
 export default instance;
